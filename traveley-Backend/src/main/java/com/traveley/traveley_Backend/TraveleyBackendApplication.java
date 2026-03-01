be5364140cfc860101f9172e95e8294409c4320e
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-01 07:34:45 +0530

    chore(setup): create sprinboot file

---
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/TraveleyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.traveley.traveley_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveleyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveleyBackendApplication.class, args);
+	}
+
+}
---
+++ traveley-Backend/src/test/java/com/traveley/traveley_Backend/TraveleyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveley.traveley_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveleyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
