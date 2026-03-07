4efc805b5d72cb6a9cb3587a1fd9f7e0873097f9
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-07 20:43:29 +0530

    chore : update traveler profile entity

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/entity/TravelerProfile.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/entity/TravelerProfile.java
@@ -1,13 +1,13 @@
 package com.traveley.traveley_Backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -22,4 +22,11 @@ public class TravelerProfile {
     private String lastName;
     private String mobileNumber;
     private String country;
+
+    @OneToMany(mappedBy = "travelerProfile", cascade = CascadeType.ALL)
+    private List<Booking> bookings;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
