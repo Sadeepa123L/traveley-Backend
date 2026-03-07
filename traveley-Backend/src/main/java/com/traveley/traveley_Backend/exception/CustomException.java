7f564c61c5fe992ec4f2991c25c1db2cac405919
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-07 22:11:14 +0530

    feat : add custom exception class

---
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/exception/CustomException.java
@@ -0,0 +1,7 @@
+package com.traveley.traveley_Backend.exception;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
