c0f4d412d6816312edf7817e01e90d72895b12c4
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-12 00:04:28 +0530

    refactor : update local date attribute

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/entity/User.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/entity/User.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -27,6 +29,9 @@ public class User implements UserDetails {
     private String status;
     @Enumerated(EnumType.STRING)
     private Role role;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private AgencyProfile agencyProfiles;
