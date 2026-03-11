99e303ed4a7282e70eef7b8a1ad5a46f26c13151
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-11 23:56:31 +0530

    feat : add agency repository

---
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/AgencyRepo.java
@@ -0,0 +1,12 @@
+package com.traveley.traveley_Backend.repository;
+
+import com.traveley.traveley_Backend.entity.Role;
+import com.traveley.traveley_Backend.entity.User;
+import org.hibernate.engine.spi.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AgencyRepo extends JpaRepository<User, Long> {
+    List<User> findByRoleAndStatus(Role role, String status);
+}
