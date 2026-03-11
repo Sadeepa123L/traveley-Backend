e02f5c4c89c0f3cb4668924c09a97e20c7b51e42
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-12 00:04:53 +0530

    refactor : update google auth and cross

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/config/SecurityConfig.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/config/SecurityConfig.java
@@ -1,13 +1,16 @@
 package com.traveley.traveley_Backend.config;
 
+import com.traveley.traveley_Backend.util.CustomOAuth2SuccessHandler;
 import com.traveley.traveley_Backend.util.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,17 +21,25 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity
+@EnableWebSecurity
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
     private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomOAuth2SuccessHandler customOAuth2SuccessHandler) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        auth->auth.requestMatchers("/api/v1/auth/**").permitAll()
+                        auth->auth.requestMatchers("/api/v1/auth/**", "/oauth2/**").permitAll()
                                 .anyRequest().authenticated())
+
+                .oauth2Login(Customizer.withDefaults());
+                http.cors(Customizer.withDefaults())
+
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(customOAuth2SuccessHandler))
+
                 .sessionManagement(
                         session->session.sessionCreationPolicy
                                 (SessionCreationPolicy.STATELESS))
