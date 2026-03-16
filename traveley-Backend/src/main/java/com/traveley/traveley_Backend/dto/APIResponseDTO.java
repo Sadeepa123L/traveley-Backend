70083efa32f5241a8cd32f2f39ff424d7410f4ae
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-16 12:52:34 +0530

    refactor: improve internal logic and code structure

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/dto/APIResponseDTO.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/dto/APIResponseDTO.java
@@ -2,9 +2,11 @@ package com.traveley.traveley_Backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class APIResponseDTO {
     private int code;
     private String message;
