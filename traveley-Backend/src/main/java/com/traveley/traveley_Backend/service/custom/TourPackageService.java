d46df6d09237f670bd4a67c43327a021dc33ad35
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-16 21:36:38 +0530

    refactor (Service) : update Tour package update methode

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/service/custom/TourPackageService.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/service/custom/TourPackageService.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface TourPackageService {
     void addTourPackage(String username, TourPackageDTO tourPackageDTO, MultipartFile photo) throws IOException;
     List<TourPackageDTO> getPackagesForCurrentAgency(String username);
+    void updateTourPackage(Long id, String username, TourPackageDTO tourPackageDTO, MultipartFile photo) throws IOException;
+
 }
