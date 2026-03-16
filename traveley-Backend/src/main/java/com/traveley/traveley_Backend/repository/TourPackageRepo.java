34cfffa27d4ae87691b054f63da5e6ad11516b75
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-16 19:25:37 +0530

    refactor : update get tour packages methode

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/TourPackageRepo.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/TourPackageRepo.java
@@ -3,7 +3,9 @@ package com.traveley.traveley_Backend.repository;
 import com.traveley.traveley_Backend.entity.TourPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
+
 
 public interface TourPackageRepo extends JpaRepository<TourPackage, Long> {
+    List<TourPackage> findByAgencyProfile_Id(Long agencyProfileId);
 }
