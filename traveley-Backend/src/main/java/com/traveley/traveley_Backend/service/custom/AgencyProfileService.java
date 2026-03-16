66f402ee8b145fbaadcb17ca5825fceff89f223a
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-16 12:42:22 +0530

    feat (Service) :Add Agency profile Service interface

---
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/service/custom/AgencyProfileService.java
@@ -0,0 +1,10 @@
+package com.traveley.traveley_Backend.service.custom;
+
+import com.traveley.traveley_Backend.dto.AgencyProfileDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface AgencyProfileService {
+    void saveOrUpdateProfile(String username, AgencyProfileDTO agencyProfileDTO, MultipartFile profileImage) throws IOException;
+}
