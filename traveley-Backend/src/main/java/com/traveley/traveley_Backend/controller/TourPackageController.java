3ae20f7c76e416b0b827994e4e58d2143d7defe7
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-16 12:46:43 +0530

    feat (Controller) :Add Tour package Controller class

---
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/controller/TourPackageController.java
@@ -0,0 +1,41 @@
+package com.traveley.traveley_Backend.controller;
+
+import com.traveley.traveley_Backend.dto.TourPackageDTO;
+import com.traveley.traveley_Backend.service.custom.TourPackageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/tourPackage")
+public class TourPackageController {
+
+    private final TourPackageService tourPackageService;
+
+    @PostMapping(value = "/save", consumes = {"multipart/formData"})
+    public ResponseEntity<?> saveTourPackage(
+            Principal principal,
+            @RequestPart("packageData")TourPackageDTO tourPackageDTO,
+            @RequestPart(value = "photo", required = false) MultipartFile photo
+            ){
+        try{
+            tourPackageService.addTourPackage(principal.getName(), tourPackageDTO, photo);
+            return ResponseEntity.ok(Map.of("message", "Tour Package saved successfully!"));
+        }catch (IOException e){
+            return ResponseEntity.status(500).body(Map.of("error", "Image upload failed: " + e.getMessage()));
+        }catch (RuntimeException e){
+            return ResponseEntity.status(400).body(Map.of("error", e.getMessage()));
+        }catch (Exception e){
+            return ResponseEntity.status(500).body(Map.of("error", "Internal Server Error occurred."));
+        }
+    }
+}
