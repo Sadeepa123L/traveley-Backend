d06a84931bc0946de99d1a7002efc12df8b1a14c
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-18 00:39:50 +0530

    feat (Repository) : create custom query for get all tour packages in active agencies

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/TourPackageRepo.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/TourPackageRepo.java
@@ -2,10 +2,15 @@ package com.traveley.traveley_Backend.repository;
 
 import com.traveley.traveley_Backend.entity.TourPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface TourPackageRepo extends JpaRepository<TourPackage, Long> {
     List<TourPackage> findByAgencyProfile_Id(Long agencyProfileId);
+
+    @Query("SELECT t FROM TourPackage t WHERE t.agencyProfile.user.status = :status")
+    List<TourPackage> findActivePackages(@Param("status") String status);
 }
