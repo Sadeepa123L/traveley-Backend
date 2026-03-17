bcdda86969634baf0e3458cafb135c712a6cb448
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-18 00:37:42 +0530

    feat (Repository) : create a method for get all active agencies

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/AgencyProfileRepo.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/AgencyProfileRepo.java
@@ -1,10 +1,13 @@
 package com.traveley.traveley_Backend.repository;
 
 import com.traveley.traveley_Backend.entity.AgencyProfile;
+import com.traveley.traveley_Backend.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AgencyProfileRepo extends JpaRepository<AgencyProfile, Long> {
     Optional<AgencyProfile> findByUserId(Long id);
+    List<AgencyProfile> findByUser_StatusAndUser_Role(String user_status, Role user_role);
 }
