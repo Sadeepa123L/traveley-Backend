d10ffc36e58d3d62f04cd05a0d20c77bff83e8d6
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-17 08:45:56 +0530

    feat (Dto) : create password update dto for get data in frontend

---
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/dto/PasswordUpdateDTO.java
@@ -0,0 +1,13 @@
+package com.traveley.traveley_Backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PasswordUpdateDTO {
+    String currentPassword;
+    String newPassword;
+}
