67151d48fc65697ae35cca8853ef44eb55206adf
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-21 20:04:37 +0530

    feat (Repo) : some changes

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/TravelerProfileRepo.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/TravelerProfileRepo.java
@@ -1,10 +1,12 @@
 package com.traveley.traveley_Backend.repository;
 
 import com.traveley.traveley_Backend.entity.TravelerProfile;
+import com.traveley.traveley_Backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface TravelerProfileRepo extends JpaRepository<TravelerProfile, Long> {
    Optional<TravelerProfile> findByUser_Id(Long id);
+
 }
--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/UserRepo.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/UserRepo.java
@@ -3,8 +3,11 @@ package com.traveley.traveley_Backend.repository;
 import com.traveley.traveley_Backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    List<User> id(Long id);
 }
