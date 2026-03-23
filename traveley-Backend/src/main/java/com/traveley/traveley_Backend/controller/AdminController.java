e95d7fd5dc59d28581909794475dd39b89c28e26
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-23 16:28:24 +0530

    feat (Controller) : implement delete agency end point

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/controller/AdminController.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/controller/AdminController.java
@@ -1,9 +1,12 @@
 package com.traveley.traveley_Backend.controller;
 
+import com.traveley.traveley_Backend.service.custom.AgencyService;
 import com.traveley.traveley_Backend.service.custom.TravelerProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,10 +14,20 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
 
     private final TravelerProfileService travelerProfileService;
+    private final AgencyService agencyService;
 
     @DeleteMapping("/traveler/{id}")
     public ResponseEntity<String> deleteProfile(@PathVariable Long id) {
         travelerProfileService.deleteTraveler(id);
         return ResponseEntity.ok("Deleted traveler");
+    }
+    @DeleteMapping("/deleteAgency/{id}")
+    public ResponseEntity<?> deleteAgency(@PathVariable Long id){
+        try {
+            agencyService.deleteAgency(id);
+            return ResponseEntity.ok("Deleted agency");
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(Map.of("error", e.getMessage()));
+        }
     }
 }
