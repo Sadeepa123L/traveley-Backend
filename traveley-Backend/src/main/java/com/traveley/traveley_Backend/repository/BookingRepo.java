606d1ce918acc7dc6e8776d4a7bb394762226b7a
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-24 20:55:19 +0530

    feat( Repository): find all agency profile by id methode implement

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/BookingRepo.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/BookingRepo.java
@@ -3,5 +3,8 @@ package com.traveley.traveley_Backend.repository;
 import com.traveley.traveley_Backend.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepo extends JpaRepository<Booking, Long> {
+    List<Booking> findAllByAgencyProfile_Id(Long id);
 }
