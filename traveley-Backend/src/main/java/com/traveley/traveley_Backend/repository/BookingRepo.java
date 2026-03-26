4816d985a666b6a72b13b484fb4468018dfe0bb8
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-03-26 22:12:04 +0530

    refactor (Repo) : update some implementation

--- traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/BookingRepo.java
+++ traveley-Backend/src/main/java/com/traveley/traveley_Backend/repository/BookingRepo.java
@@ -3,8 +3,11 @@ package com.traveley.traveley_Backend.repository;
 import com.traveley.traveley_Backend.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public interface BookingRepo extends JpaRepository<Booking, Long> {
     List<Booking> findAllByAgencyProfile_Id(Long id);
+    List<Booking> findByTravelDateBetween(LocalDate startDate, LocalDate endDate);
 }
